bc86b63a5ea6217b5fe9a8f8a45b6dd3be40eeef
Fumikazu Fujiwara <github@ze.gs>
2020-04-04 10:11:55 +0900

    add compile

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-for i in `find . -type f -iname *.als`; 
-  do java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i 2> /dev/null ; echo $i ;
+export RET=0
+for i in `find . -type f -iname *.als`; do 
+    java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i 2> /dev/null > output ; 
+    if grep -q Unsatisfiable output ;
+    then
+        echo "Unsatisfiable $i"
+        RET=1
+    else
+        cat output
+    fi
+    rm output
 done
+exit $RET
