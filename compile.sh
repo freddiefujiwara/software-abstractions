6c2c25ed5ae25897ad4a27a00a3dcf9b47031224
Freddie FUJIWARA <github@ze.gs>
2020-04-04 09:59:39 +0900

    add echo

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for i in `find . -type f -iname *.als`; 
-  do java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i 2> /dev/null;
+  do java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i 2> /dev/null ; echo $i ;
 done
