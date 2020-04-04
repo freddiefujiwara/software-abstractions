a5c1eeb8742390d87a1fdf0ce4fbc2dd933c2c72
Fumikazu Fujiwara <github@ze.gs>
2020-04-04 10:39:15 +0900

    Unsatisfiable -> error

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export RET=0
 for i in `find . -type f -iname *.als`; do 
-    java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i 2> /dev/null > output ; 
-    if grep -q Unsatisfiable output ;
+    java -cp org.alloytools.alloy.dist.jar edu.mit.csail.sdg.alloy4whole.ExampleUsingTheCompiler $i > output 2>&1; 
+    if grep -q error output ;
     then
-        echo "Unsatisfiable $i"
+        echo "error $i"
         RET=1
     else
         cat output
